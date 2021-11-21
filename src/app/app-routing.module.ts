55d7562697b01bdd1423862e1bcb3a7bece80afc
nathaliacteixeira <teixeira.nathalia.c@gmail.com>
2021-11-21 18:01:52 -0300

    alterando as pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,18 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'historico',
+    loadChildren: () => import('./gastos/historico/historico.module').then( m => m.HistoricoPageModule)
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./gastos/inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'adicionar',
+    loadChildren: () => import('./gastos/adicionar/adicionar.module').then( m => m.AdicionarPageModule)
   }
 ];
 @NgModule({
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -8,27 +8,27 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'historico',
+        loadChildren: () => import('../gastos/historico/historico.module').then(m => m.HistoricoPageModule)
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        path: 'inicio',
+        loadChildren: () => import('../gastos/inicio/inicio.module').then(m => m.InicioPageModule)
       },
       {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        path: 'adicionar',
+        loadChildren: () => import('../gastos/adicionar/adicionar.module').then(m => m.AdicionarPageModule)
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/inicio',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/inicio',
     pathMatch: 'full'
   }
 ];
